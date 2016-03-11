ebad73e6d394042ddb667f3ce83919a7b6be3a01
Samantha Hoy <samantha.hoy@gmail.com>
2016-03-11 14:27:44 +0200

    Added matching and displaying on rows and columns

--- script.js
+++ script.js
@@ -44,41 +44,50 @@ function dispMatchingNumberRow (row,mesg,num){
 	document.getElementById(mesg).innerHTML =
     "In row" + row + " matching numbers found for " + num ;
 };
+function dispMatchingNumberCol (col,mesg,num){
+  console.log("In col" + col + " matching numbers found for " + num);
+	document.getElementById(mesg).innerHTML =
+    "In col" + col + " matching numbers found for " + num ;
+};
 
-var rowNo = 0;
+// var rowNo = 0;
 var numMatched = 0;
 var numMatched2 = 0;
 var numMatched3 = 0;
+var numMatched4 = 0;
+var numMatched5 = 0;
+var numMatched6 = 0;
+var numMatched7 = 0;
+var numMatched8 = 0;
+var numMatched9 = 0;
 // var noOfMatches = 0;
 
 var element1 = document.getElementById("divSection1");
-element1.innerHTML = getRandomIntInclusive(1,9);
+element1.innerHTML = getRandomIntInclusive(1,3);
 
 var element2 = document.getElementById("divSection2");
-element2.innerHTML = getRandomIntInclusive(1,9);
+element2.innerHTML = getRandomIntInclusive(1,3);
 
 var element3 = document.getElementById("divSection3");
-element3.innerHTML = getRandomIntInclusive(1,9);
+element3.innerHTML = getRandomIntInclusive(1,3);
 
 var element4 = document.getElementById("divSection4");
-element4.innerHTML = getRandomIntInclusive(1,9);
+element4.innerHTML = getRandomIntInclusive(1,3);
 
 var element5 = document.getElementById("divSection5");
-element5.innerHTML = getRandomIntInclusive(1,9);
+element5.innerHTML = getRandomIntInclusive(1,3);
 
 var element6 = document.getElementById("divSection6");
-element6.innerHTML = getRandomIntInclusive(1,9);
+element6.innerHTML = getRandomIntInclusive(1,3);
 
 var element7 = document.getElementById("divSection7");
-element7.innerHTML = getRandomIntInclusive(1,9);
+element7.innerHTML = getRandomIntInclusive(1,3);
 
 var element8 = document.getElementById("divSection8");
-element8.innerHTML = getRandomIntInclusive(1,9);
+element8.innerHTML = getRandomIntInclusive(1,3);
 
 var element9 = document.getElementById("divSection9");
-element9.innerHTML = getRandomIntInclusive(1,9);
-
-
+element9.innerHTML = getRandomIntInclusive(1,3);
 
 // document.getElementById("myButton").addEventListener("click", reloadPage);
 document.getElementById("myButton").addEventListener("click", function(){
@@ -91,28 +100,22 @@ if (element1.innerHTML === element2.innerHTML) {
     document.getElementById("divSection2").classList.add("highlight");
     numMatched = document.getElementById("divSection1").innerHTML ;
     // noOfMatches++;
-    if (numMatched !== 0) {
-      dispMatchingNumberRow(1,"message1",numMatched);
-    }
-  }
+    dispMatchingNumberRow(1,"message1",numMatched);
+}
 if (element2.innerHTML === element3.innerHTML) {
     document.getElementById("divSection2").classList.add("highlight");
     document.getElementById("divSection3").classList.add("highlight");
     numMatched = document.getElementById("divSection2").innerHTML ;
     // noOfMatches++;
-    if (numMatched !== 0) {
-      dispMatchingNumberRow(1,"message1",numMatched);
-    }
-  }
+    dispMatchingNumberRow(1,"message1",numMatched);
+}
 if (element1.innerHTML === element3.innerHTML) {
     document.getElementById("divSection1").classList.add("highlight");
     document.getElementById("divSection3").classList.add("highlight");
     numMatched = document.getElementById("divSection3").innerHTML ;
     // noOfMatches++;
-    if (numMatched !== 0) {
-      dispMatchingNumberRow(1,"message1",numMatched);
-    	}
-	}
+    dispMatchingNumberRow(1,"message1",numMatched);
+}
 
 // row2
 if (element4.innerHTML === element5.innerHTML) {
@@ -121,29 +124,22 @@ if (element4.innerHTML === element5.innerHTML) {
     document.getElementById("divSection5").classList.add("highlight");
     numMatched2 = document.getElementById("divSection4").innerHTML ;
     // noOfMatches++;
-    if (numMatched2 !== 0) {
-      dispMatchingNumberRow(2,"message2",numMatched2);
-    	}
-  }
+    dispMatchingNumberRow(2,"message2",numMatched2);
+}
 if (element5.innerHTML === element6.innerHTML) {
     document.getElementById("divSection5").classList.add("highlight");
     document.getElementById("divSection6").classList.add("highlight");
     numMatched2 = document.getElementById("divSection5").innerHTML ;
     // noOfMatches++;
-    if (numMatched2 !== 0) {
-      dispMatchingNumberRow(2,"message2",numMatched2);
-    	}
-  }
-
+    dispMatchingNumberRow(2,"message2",numMatched2);
+}
 if (element4.innerHTML === element6.innerHTML) {
     document.getElementById("divSection4").classList.add("highlight");
     document.getElementById("divSection6").classList.add("highlight");
     numMatched2 = document.getElementById("divSection4").innerHTML ;
     // noOfMatches++;
-    if (numMatched2 !== 0) {
-      dispMatchingNumberRow(2,"message2",numMatched2);
-    	}
-  }
+    dispMatchingNumberRow(2,"message2",numMatched2);
+}
 
 // row3
 if (element7.innerHTML === element8.innerHTML) {
@@ -152,28 +148,91 @@ if (element7.innerHTML === element8.innerHTML) {
     document.getElementById("divSection8").classList.add("highlight");
     numMatched3 = document.getElementById("divSection7").innerHTML ;
     // noOfMatches++;
-    if (numMatched3 !== 0) {
-      dispMatchingNumberRow(3,"message3",numMatched3);
-    	}
-  }
-if (element8.innerHTML === element9.innerHTML) {
+    dispMatchingNumberRow(3,"message3",numMatched3);
+}
+    if (element8.innerHTML === element9.innerHTML) {
     document.getElementById("divSection8").classList.add("highlight");
     document.getElementById("divSection9").classList.add("highlight");
     numMatched3 = document.getElementById("divSection8").innerHTML ;
     // noOfMatches++;
-    if (numMatched3 !== 0) {
-      dispMatchingNumberRow(3,"message3",numMatched3);
-    	}
-  }
-
+    dispMatchingNumberRow(3,"message3",numMatched3);
+}
 if (element7.innerHTML === element9.innerHTML) {
     document.getElementById("divSection7").classList.add("highlight");
     document.getElementById("divSection9").classList.add("highlight");
     numMatched3 = document.getElementById("divSection7").innerHTML ;
     // noOfMatches++;
-    if (numMatched3 !== 0) {
-      dispMatchingNumberRow(3,"message3",numMatched3);
-    	}
-  }
+    dispMatchingNumberRow(3,"message3",numMatched3);
+}
+//  column 1
+if (element1.innerHTML === element4.innerHTML) {
+    document.getElementById("divSection1").classList.add("highlight");
+    document.getElementById("divSection4").classList.add("highlight");
+    numMatched4 = document.getElementById("divSection1").innerHTML ;
+    // noOfMatches++;
+    dispMatchingNumberCol(1,"message4",numMatched4);
+}
+if (element4.innerHTML === element7.innerHTML) {
+    document.getElementById("divSection4").classList.add("highlight");
+    document.getElementById("divSection7").classList.add("highlight");
+    numMatched4 = document.getElementById("divSection4").innerHTML ;
+    // noOfMatches++;
+      dispMatchingNumberCol(1,"message4",numMatched4);
+}
+if (element1.innerHTML === element7.innerHTML) {
+    document.getElementById("divSection1").classList.add("highlight");
+    document.getElementById("divSection7").classList.add("highlight");
+    numMatched4 = document.getElementById("divSection1").innerHTML ;
+    // noOfMatches++;
+      dispMatchingNumberCol(1,"message4",numMatched4);
+}
 
-  
+// column 2
+
+if (element2.innerHTML === element5.innerHTML){
+  document.getElementById("divSection2").classList.add("highlight");
+  document.getElementById("divSection5").classList.add("highlight");
+  numMatched5 = document.getElementById("divSection2").innerHTML ;
+  dispMatchingNumberCol(2,"message5",numMatched5);
+}
+if (element5.innerHTML === element8.innerHTML){
+  document.getElementById("divSection5").classList.add("highlight");
+  document.getElementById("divSection8").classList.add("highlight");
+  numMatched5 = document.getElementById("divSection5").innerHTML;
+  dispMatchingNumberCol(2,"message5",numMatched5);
+}
+if (element2.innerHTML === element8.innerHTML){
+  document.getElementById("divSection2").classList.add("highlight");
+  document.getElementById("divSection8").classList.add("highlight");
+  numMatched5 = document.getElementById("divSection2").innerHTML ;
+  dispMatchingNumberCol(2,"message5",numMatched5);
+}
+
+// column 3
+
+if (element3.innerHTML === element6.innerHTML){
+  document.getElementById("divSection3").classList.add("highlight");
+  document.getElementById("divSection6").classList.add("highlight");
+  numMatched6 = document.getElementById("divSection3").innerHTML ;
+  dispMatchingNumberCol(3,"message6",numMatched6);
+}
+if (element6.innerHTML === element9.innerHTML){
+  document.getElementById("divSection6").classList.add("highlight");
+  document.getElementById("divSection9").classList.add("highlight");
+  numMatched6 = document.getElementById("divSection6").innerHTML;
+  dispMatchingNumberCol(3,"message6",numMatched6);
+}
+if (element3.innerHTML === element9.innerHTML){
+  document.getElementById("divSection3").classList.add("highlight");
+  document.getElementById("divSection9").classList.add("highlight");
+  numMatched6 = document.getElementById("divSection2").innerHTML ;
+  dispMatchingNumberCol(3,"message6",numMatched6);
+}
+
+// diagonal top left to bottom right
+
+if (element1.innerHTML === element5.innerHTML){
+  document.getElementById("divsection1").classList.add("highlight");
+  document.getElementById("divSection5").classList.add("highlight");
+
+}
